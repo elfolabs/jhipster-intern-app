dd8be6a0290b3bd2ba9dc5942e600b4c4b6829a3
igorbastos <igor.oliveira@singletech.com.br>
2023-09-15 22:04:52 -0300

    atividades do live Coding feitas

--- src/main/java/br/com/elfotec/repository/MediaFileRepository.java
+++ src/main/java/br/com/elfotec/repository/MediaFileRepository.java
@@ -1,12 +1,15 @@
 package br.com.elfotec.repository;
 
 import br.com.elfotec.domain.MediaFile;
-import org.springframework.data.jpa.repository.*;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-/**
- * Spring Data SQL repository for the MediaFile entity.
- */
-@SuppressWarnings("unused")
 @Repository
-public interface MediaFileRepository extends JpaRepository<MediaFile, Long> {}
+public interface MediaFileRepository extends JpaRepository<MediaFile, Long> {
+    @Query("SELECT mf FROM MediaFile mf WHERE mf.dataExclusao IS NULL")
+    List<MediaFile> findAllWithNullDataExclusao();
+}
